9e022f8230b94491549d58a019389d4efa139200
Leandro D. Medus <leomedus@gmail.com>
2020-04-16 09:42:30 +0200

    [mod] word length in top as generic

--- basics/2_abacus_demo/src/top_abacus.vhd
+++ basics/2_abacus_demo/src/top_abacus.vhd
@@ -26,7 +26,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;               -- ieee 1076.3
 
 entity top is
-    Port (
+    generic (
+        g_WORD_WIDTH    => 7
+    )
+    port (
         -- global input signals
         clk_i       : in  std_logic;        -- system clock
         rst_n_i     : in std_logic;         -- global reset (active low)
@@ -37,24 +40,38 @@ entity top is
         btn_right_i : in std_logic;
         btn_center_i: in std_logic;
         -- switches
-        sw_n1_i       : in std_logic_vector (6 downto 0);
-        sw_n2_i       : in std_logic_vector (6 downto 0);
+        sw_n1_i       : in std_logic_vector (g_WORD_WIDTH-1 downto 0);
+        sw_n2_i       : in std_logic_vector (g_WORD_WIDTH-1 downto 0);
 
         -- global output signals
         -- leds
         led_on_o    : out std_logic;        -- led reset disable
-        leds_n1_o      : out std_logic_vector (6 downto 0);
-        leds_n2_o      : out std_logic_vector (6 downto 0)
+        leds_n1_o      : out std_logic_vector (g_WORD_WIDTH-1 downto 0);
+        leds_n2_o      : out std_logic_vector (g_WORD_WIDTH-1 downto 0)
     );
 end top;
 
 architecture Behavioral of top is
     -- signal declarations
+    signal s_result_sum : std_logic_vector (g_WORD_WIDTH-1 downto 0);
 
     -- module declarations
 
 begin
     -- module initializations
+    ADDER0: entity work.adder
+    generic map(
+        g_WORD_WIDTH    => g_WORD_WIDTH
+    )
+    port map (
+        clk_i           => clk_i,
+        rst_n_i         => rst_n_i,
+        operand_1_i     => sw_n1_i,
+        operand_2_i     => sw_n2_i,
+        valid_o         => open,
+        result_o        => s_result_sum,
+        overflow_o      => open
+    );
 
     p_top_behav : process(rst_n_i, clk_i)
     begin
