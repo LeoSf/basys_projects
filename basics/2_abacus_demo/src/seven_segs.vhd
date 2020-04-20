88fa1b3b6d57050bb4b86cb8efd7e0dda870eabf
Leandro D. Medus <leomedus@gmail.com>
2020-04-21 01:46:03 +0200

    [add] binary to bcd converter module (FSM) [add] binary to bcd digit logic [mod] seven_seg: binary to bcd module instantiation [del] old binary to bcd file

--- basics/2_abacus_demo/src/seven_segs.vhd
+++ basics/2_abacus_demo/src/seven_segs.vhd
@@ -16,7 +16,12 @@
 --      04/16/2020  v0.01 File created
 --
 -- Additional Comments:
---      This is a basic module to display only numbers on the Basys3 board.
+--      This is a basic module to display only numbers on the seven segments
+--      present on the Basys3 board.
+--
+-- TODOs:
+--      * implement digital point on each segment.
+--      * enable signal
 ----------------------------------------------------------------------------------
 
 ----------------------------------------------------------------------------------
@@ -35,7 +40,8 @@ entity seven_segs is
         clk_i           : in std_logic;         -- system clock
         rst_n_i         : in std_logic;         -- global reset (active low)
         en_i            : in std_logic;         -- enable signal
-        display_value_i : in std_logic_vector (g_N_BITS-1 downto 0);  -- value to be displayed
+        value_i         : in std_logic_vector (g_N_BITS-1 downto 0);  -- value to be displayed
+        dp_i           : in std_logic_vector (g_N_SEGMENTS-1 downto 0)
 
         -- global output signals
         sseg_ca_o 		: out  std_logic_vector (7 downto 0);
@@ -45,10 +51,10 @@ end seven_segs;
 
 architecture behavioral of seven_segs is
     -- constant
-    -- constant c_TMR_CNTR_MAX : std_logic_vector(26 downto 0) := "101111101011110000100000000"; --100,000,000 = clk cycles per second
-    -- constant c_TMR_VAL_MAX : std_logic_vector(3 downto 0) := "1001"; --9
-    constant c_TMR_CNTR_MAX : unsigned(26 downto 0) := "101111101011110000100000000"; --100,000,000 = clk cycles per second
-    constant c_TMR_VAL_MAX : unsigned(3 downto 0) := "1001"; --9
+    -- timer max counter for a 1 MHz clock: 100,000,000 = clk cycles per second
+    constant c_TMR_CNTR_MAX : unsigned(26 downto 0) := "101111101011110000100000000";
+    -- max value to display in the 7-seg: 9
+    constant c_TMR_VAL_MAX : unsigned(3 downto 0) := "1001";
 
     -- Signals
     --This is used to determine when the 7-segment display should be
@@ -57,10 +63,28 @@ architecture behavioral of seven_segs is
 
     --This counter keeps track of which number is currently being displayed
     --on the 7-segment. #TODO generics
-    -- signal digit_val : std_logic_vector(3 downto 0) := (others => '0');
     signal digit_val : unsigned (3 downto 0) := (others => '0');
 
+
+    signal values_bcd   :	std_logic_vector (g_N_SEGMENTS * 4 - 1 DOWNTO 0));
+    signal module_ena   : std_logic;
+    signal module_busy  : std_logic;
 begin
+
+    ADDER0: entity work.binary_to_bcd
+    generic map(
+        bits    => g_N_BITS;
+        digits  => g_N_SEGMENTS
+    )
+    port map (
+        clk         => clk_i,
+        reset_n     => rst_n_i,
+        ena         => open,
+        binary      => value_i,
+        busy        => module_busy,
+        bcd         => values_bcd
+    );
+
 
     p_7seg_behav : process(rst_n_i, clk_i)
     begin
@@ -79,17 +103,20 @@ begin
     -- Encoding the current value to display to the necessary cathode
     -- signals to display n the seven segment digit (296)
     with digit_val select
-    	sseg_ca_o <=  "01000000" when "0000",
-                    "01111001" when "0001",
-                    "00100100" when "0010",
-                    "00110000" when "0011",
-                    "00011001" when "0100",
-                    "00010010" when "0101",
-                    "00000010" when "0110",
-                    "01111000" when "0111",
-                    "00000000" when "1000",
-                    "00010000" when "1001",
-                    "11111111" when others; -- default: leds off
+        sseg_ca_o(6 downto 0) <= "1000000" when "0000",
+            "1111001" when "0001",  -- 1
+            "0100100" when "0010",  -- 2
+            "0110000" when "0011",  -- 3
+            "0011001" when "0100",  -- 4
+            "0010010" when "0101",  -- 5
+            "0000010" when "0110",  -- 6
+            "1111000" when "0111",  -- 7
+            "0000000" when "1000",  -- 8
+            "0010000" when "1001",  -- 9
+            "1111111" when others;  -- default: leds off
+
+    -- disabling digital point for all digits
+    sseg_ca_o(7) <= '1';
 
     --This process controls the counter that triggers the 7-segment
     --to be incremented. It counts 100,000,000 and then resets.
@@ -97,13 +124,9 @@ begin
     begin
         if rst_n_i = '0'  then      -- asynchronous reset
             tmrCntr <= (others => '0');
-            -- tmrCntr <= 0;
-            -- tmrVal <= (others => '0');
-
         elsif (rising_edge(clk_i)) then
     		if (tmrCntr = c_TMR_CNTR_MAX) then
     			tmrCntr <= (others => '0');
-                -- tmrCntr <= 0;
     		else
     			tmrCntr <= tmrCntr + 1;
     		end if;
@@ -111,11 +134,11 @@ begin
     end process;
 
     --This process increments the digit being displayed on the
-    --7-segment display every second.
-    timer_inc_process : process (rst_n_i, clk_i)
+    --7-segment display every second bacues clk is 100 MHz
+    digit_inc_process : process (rst_n_i, clk_i)
     begin
         if rst_n_i = '0'  then      -- asynchronous reset
-            -- tmrVal <= (others => '0');
+            digit_val <= (others => '0');
 
     	elsif (rising_edge(clk_i)) then
     		if (tmrCntr = c_TMR_CNTR_MAX) then
